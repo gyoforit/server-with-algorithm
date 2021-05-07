0d722f285056df58104ab47e4360dd02d2b5e87d
gyoforit <gyoforit@gmail.com>
2021-05-07 17:06:45 +0900

    Complete project

--- movies/views.py
+++ movies/views.py
@@ -25,13 +25,16 @@ def detail(request, movie_pk):
 
 @require_GET
 def recommended(request):
-    # 출시월이 현재 검색하는 달과 같은 영화를 추천해주려 한다.
-    today = datetime.today()
-    movies = Movie.objects.filter(release_date__month=today)
-    
+    # 개봉월이 현재 검색하는 달과 같은 영화를 추천해주려 한다.
+    today_month = datetime.today().month
+    movies = Movie.objects.filter(release_date__month=today_month)
+    from_now = dict()
+    for movie in movies:
+        from_now[movie.pk] = datetime.today().year-movie.release_date.year
     context = {
-        'today': today,
+        'today_month': today_month,
         'movies': movies,
+        'from_now': from_now,
     }
 
     return render(request, 'movies/recommended.html', context)
